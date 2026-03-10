5199f9249901d2114554afc5ae4a898fb23f1a7b
1daymanymood <yossapong.nakp@hotmail.com>
2026-03-10 19:00:24 +0700

    edit

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import pokeball from '../assets/Pokeball-PNG-image.png';
+import pokeball from '../assets/Pokeball-PNG-Image.png';
 const LoadingAnimation = ({ size = 16 }) => {
     return (
         <div className='flex justify-center items-center min-h-screen'>
